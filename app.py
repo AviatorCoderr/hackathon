29e059d3265ddc6b578267699a55b58471fcd2a4
AviatorCoderr <124370316+AviatorCoderr@users.noreply.github.com>
2023-09-10 04:55:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+# app.py
 import streamlit as st
-st.title('Mark your attendance')
-option = st.selectbox(
-    'how would you like to be contacted?',
-    {'Email', 'Mummy Papa', 'Mobile'}
-)
+import cv2
+
+st.title("OpenCV and Streamlit Example")
+
+# Upload an image
+uploaded_image = st.file_uploader("Upload an image", type=["jpg", "png", "jpeg"])
+
+if uploaded_image:
+    image = cv2.imread(uploaded_image.name)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
